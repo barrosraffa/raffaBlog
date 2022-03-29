539baa944ef4583ae4ffeaea4cf8fc3602a0e495
barrosraffa <rafaellaponci@gmail.com>
2022-03-29 18:10:58 -0300

    Carrossel add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import Nav from './componentes/Nav'
+import Nav from './componentes/Nav';
+import Carrossel from './componentes/carrossel'
 
 
 function App() {
   return (
     <Fragment>
       <Nav />
+      <Carrossel />
     </Fragment>
   );
 }
---
+++ src/componentes/carrossel/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import './carrossel.scss'
+import Gustavo from '../../assets/Gustavito.jpg';
+import Gu from '../../assets/Gustavo-0368.jpg'
+
+
+class Carrossel extends Component {
+    render(){
+        return(
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src={Gu} id="imagem"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={Gu} id="imagem"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={Gustavo} id="imagem"/>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            
+        )
+    }
+}
+
+export default Carrossel
