c72a214ab637603f4c6ac0918a54eedde8ecdfe6
barrosraffa <rafaellaponci@gmail.com>
2022-03-29 17:30:12 -0300

    nav bar ok

--- src/componentes/Nav/index.js
+++ src/componentes/Nav/index.js
@@ -4,12 +4,37 @@ import "./nav.scss"
 class Nav extends Component {
     render(){
         return(
-            <nav className="nav_bar">
-                <a href="#">Home</a>
-                <a href="#Potfolio">Potfolio</a>
-                <a href="#Faq">Faq</a>
-                <a href="#Contato">Contato</a>
-            </nav>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    {/* <a class="navbar-brand" href="#Home">Home</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button> */}
+                    <div class="nav-teste collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#contato">Contato</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#sobre">Sobre</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#Faq">Faq</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#Portfolio" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Portfolio
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#ensaio">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+</nav>
         )
     }  
 }
